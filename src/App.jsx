20524ee6474e3afd56460c82fda5f6a26a294aa5
Matias Herrera Posse <105301287+Mati138@users.noreply.github.com>
2025-07-24 23:56:38 -0300

    Primera subida

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Components/header/Header"
+import Footer from "./Components/footer/Footer"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <h1>Craft Store</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Soy el Header</h1>
+    </div>
+  )
+}
+
+export default Header
+
